9ab5d83912019766f4f60bcc7e8422f24d4c2268
mkbonini <mkbonini@gmail.com>
2022-05-17 14:26:22 -0600

    Add test for empty? in cell spec

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe Cell do
 
     expect(cell.coordinate).to eq("B4")
   end
+  it 'has a ship' do
+    cell = Cell.new("B4")
+
+    expect(cell.ship).to eq(nil)
+  end
+
+  it 'empty?' do
+    cell = Cell.new("B4")
+
+    expect(cell.empty?).to be true
+  end
 end
