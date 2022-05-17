7c691be3d24e7507712a255797fb6ecb6061bf65
Andre Pedro <andrerpedro170@gmail.com>
2022-05-17 11:18:20 -0600

    add length test

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -3,12 +3,22 @@ require 'rspec'
 
 RSpec.describe Ship do
   it 'exists' do
-  cruiser = Ship.new("Cruiser", 3)
+    cruiser = Ship.new("Cruiser", 3)
 
-  expect(cruiser).to be_a(Ship)
+    expect(cruiser).to be_a(Ship)
+  end
 
+  it 'name' do
+    cruiser = Ship.new("Cruiser", 3)
 
-end
+    expect(cruiser.name).to eq("Cruiser")
+  end
+
+  it 'length' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.length).to eq(3)
+  end
 
 
 
