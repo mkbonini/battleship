6b94b7f575571ddb0a7ca1c6ce871229d75005d7
Andre Pedro <andrerpedro170@gmail.com>
2022-05-17 11:14:51 -0600

    add first ship test

---
+++ spec/ship_spec.rb
@@ -0,0 +1,24 @@
+require './lib/ship'
+require 'rspec'
+
+RSpec.describe Ship do
+  it 'exists' do
+  cruiser = Ship.new("Cruiser", 3)
+
+  expect(cruiser).to be_a(Ship)
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+end
