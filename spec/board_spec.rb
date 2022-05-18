9e8aba4fd34c63a069abea80ce1e343708c3bd6c
Andre Pedro <andrerpedro170@gmail.com>
2022-05-18 11:22:40 -0600

    create vaalildate coordinates test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -18,6 +18,14 @@ RSpec.describe Board do
 
   end
 
+  it 'validates coordinates' do
+    board= Board.new
+    expect(board.valid_coordinate?("A1")).to be true
+    expect(board.valid_coordinate?("D4")).to be true
+    expect(board.valid_coordinate?("A5")).to be false
+    expect(board.valid_coordinate?("E1")). to be false
+    expect(board.valid_coordinate?("A22")).to be false
+  end
 
 
 
