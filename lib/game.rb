69d3649e38e6549eb2f138ae5eb0c8ef7cd1115b
Andre Pedro <andrerpedro170@gmail.com>
2022-05-23 14:05:26 -0600

    add feature to place player ships

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,13 @@
+require './lib/game'
+require './lib/computer_player'
 class Game
 
   def initialize
-    computer = ComputerPlayer.new
+    @computer = ComputerPlayer.new
+    @board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    @ships = [cruiser, submarine]
   end
 
   def start
@@ -22,8 +28,29 @@ class Game
     end
 
   def setup
-    computer.ship_placement
+    @computer.ship_placement
+    puts "I have laid out my ships on the grid."
+    puts "You now need to lay out your two ships."
+    puts "The Cruiser is three units long and the Submarine is two units long."
+    puts @board.render
+
+    @ships.each do |ship|
+      puts "Enter the square for the #{ship.name} (#{ship.length} spaces)"
+      input = gets.chomp.upcase
+      input_array = input.split(" ")
+      until @board.valid_placement?(ship, input_array) do
+        puts "Those are invalid coordinates. Please try again"
+        input = gets.chomp.upcase
+        input_array = input.split(" ")
+      end
+      @board.place(ship, input_array)
+      puts @board.render(true)
+
+    end
+
     #puts computer.board.render(true)
     #puts "got to setup"
   end
+
+
 end
